ea7cf5613f6350973064251ccb17f831438d7f94
JamesDaniels15 <Dj2278524@gmail.com>
2022-09-09 07:52:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+* main - print the string in the put function
+*
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzel
+* Return: 0
+*/
+int main(void)
+    puts("\"programming is like building a multilingual puzzel");
+    return (0);
